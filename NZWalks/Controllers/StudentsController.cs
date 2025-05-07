8dea5e1804dca3e8b1b1e7e492d2bf3b06da1d24
Nam Cap <capmanhnam@gmail.com>
2025-05-08 00:34:19 +1200

    Add StudentsController with Get method test

---
+++ NZWalks/Controllers/StudentsController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace NZWalks.Controllers
+{
+    // this url is: https://localhost:portnumber/api/students
+    // the reason we know /students is because of the name of the controller is StudentsController
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentsController : ControllerBase
+    {
+        //GET: https://localhost:portnumber/api/students
+        [HttpGet]
+        public IActionResult GetAllStudents()
+        {
+            // this is a hardcoded list of students
+            string[] studentNames = new string[]
+            {
+                "John Doe",
+                "Jane Smith",
+                "Sam Brown",
+                "Lisa White"
+            };
+            return Ok(studentNames);
+        }
+    }
+}
